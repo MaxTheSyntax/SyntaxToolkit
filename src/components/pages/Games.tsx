10e3204b46cdd9d129b228a2363d8c350e6029b0
MaxTheSynatx <maks@x-net.pl>
2023-05-31 21:45:49 +0200

    Missing cover art now shows up as hl2

--- src/components/pages/Games.tsx
+++ src/components/pages/Games.tsx
@@ -4,7 +4,7 @@ import '../../styles/Games.css';
 
 function Games() {
 	const [games, setGames] = useState<any[]>([]);
-	const [missingCover, setMissingCover] = useState<string>('');
+	const [coverUrls, setCoverUrls] = useState<string[]>([]);
 	const [steamRes, setSteamRes] = useState<{
 		map: any;
 		game_count: number;
@@ -12,36 +12,36 @@ function Games() {
 	} | null>(null);
 
 	useEffect(() => {
-		async function fetchAllGames() {
-			try {
-				const res = await axios.get('http://localhost:8800/games');
-				setGames(res.data);
-			} catch (err) {
-				console.log(err);
-			}
-		}
-
-		async function fetchMissingCover() {
-			try {
-				setMissingCover(getCover());
-			} catch (err) {
-				console.log(err);
-			}
-		}
-
 		async function fetchSteamAPIres() {
 			try {
 				const APIres = await axios.get('http://localhost:8800/steamapi');
 				setSteamRes(APIres.data);
+
+				const coverPromises = APIres.data.games.map((game: { appid: number }) => getCover(game.appid));
+				const coverUrls = await Promise.all(coverPromises);
+				setCoverUrls(coverUrls);
 			} catch (err) {
 				console.log(err);
 			}
 		}
 
-		fetchAllGames();
-		fetchMissingCover();
 		fetchSteamAPIres();
 	}, []);
+
+	async function getCover(appid: number) {
+		const url = `https://cdn.cloudflare.steamstatic.com/steam/apps/${appid}/hero_capsule.jpg`;
+		const tempCover = 'https://cdn.cloudflare.steamstatic.com/steam/apps/440/hero_capsule.jpg';
+		try {
+			const response = await axios.get(url);
+			if (response.status === 404) {
+				return tempCover;
+			} else {
+				return url;
+			}
+		} catch (error) {
+			return tempCover;
+		}
+	}
 
 	return (
 		<div className="gamesPage">
@@ -49,25 +49,22 @@ function Games() {
 				<h1 className="title">Games ({steamRes?.game_count})</h1>
 			</center>
 			<div className="games">
-				{steamRes?.games.map(
-					(
-						game // Make a div for each game
-					) => (
-						<div key={game.appid} className="game">
-							{game.cover && <img className="gameCover" src={getCover() !== 'missing' ? `src/assets/${game.cover}` : missingCover} alt={`Cover art of ${game.title}`} />}
-							<form className="playButton" action={`steam://launch/${game.appid}`} method="POST">
-								<input key={game.appid} type="submit" value={game.appid} />
-							</form>
-						</div>
-					)
-				)}
+				{/* Make a div for each game */}
+				{steamRes?.games.map((game, index) => (
+					<div key={game.appid} className="game">
+						<img
+							className="gameCover"
+							src={coverUrls[index] === 'https://cdn.cloudflare.steamstatic.com/steam/apps/440/hero_capsule.jpg' ? 'https://cdn.cloudflare.steamstatic.com/steam/apps/220/hero_capsule.jpg' : coverUrls[index]}
+							alt={`Cover art of ${game.title}`}
+						/>
+						<form className="playButton" action={`steam://launch/${game.appid}`} method="POST">
+							<input key={game.appid} type="submit" value={game.appid} />
+						</form>
+					</div>
+				))}
 			</div>
 		</div>
 	);
-}
-
-function getCover() {
-	return 'https://cdn.cloudflare.steamstatic.com/steam/apps/440/header.jpg';
 }
 
 export default Games;
