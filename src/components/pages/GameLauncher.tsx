52ee0b97cee9b78dde0b3425000a175f712fadf6
MaxTheSyntax <maks@x-net.pl>
2024-07-06 00:44:24 +0200

    restyled game launcher page

--- src/components/pages/GameLauncher.tsx
+++ src/components/pages/GameLauncher.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
+import '../../styles/GameLauncher.css';
 
 function GameLauncher() {
 	const [gameAppId, setGameAppId] = useState<string | null>(null);
+	const [platform, setPlatform] = useState<string | null>(null);
 
 	useEffect(() => {
 		const socket = io('http://localhost:8800', {
@@ -13,14 +15,15 @@ function GameLauncher() {
 		socket.on('rungame', (data: { appid: string; platform: string }) => {
 			const { appid, platform } = data;
 			console.log(`platform: ${platform}, appid: ${appid}`);
-			if (appid) {
+			if (appid && platform) {
 				if (platform === 'steam') {
-					setGameAppId(appid);
 					window.location.href = `steam://launch/${appid}`;
 				} else if (platform === 'roblox') {
-					setGameAppId(appid);
 					window.location.href = `roblox://placeId=${appid}`;
 				}
+				setGameAppId(appid);
+				setPlatform(platform);
+				addActionToDiv(appid, platform);
 			}
 		});
 
@@ -30,12 +33,28 @@ function GameLauncher() {
 		};
 	}, []);
 
+	function addActionToDiv(appid: string, platform: string) {
+		const actionContainer = document.getElementsByClassName('action-container')[0];
+		if (actionContainer) {
+			actionContainer.innerHTML += `<p>Opened ${appid} on ${platform}</p>`;
+		}
+	}
+
 	return (
 		<div className='App'>
 			<header className='App-header'>
 				<h1>Remote App Opener</h1>
-				<p>Listening for commands...</p>
-				{gameAppId && <p>Opening {gameAppId}...</p>}
+				{!gameAppId && <p className='listen-text'>Listening for commands...</p>}
+				{gameAppId && (
+					<p className='last-action'>
+						Last action: Opened {gameAppId} on {platform}
+					</p>
+				)}
+				<div className='action-container'>
+					{/* {actions.map((element, index) => (
+						<p key={index}>Opened {element}</p>
+					))} */}
+				</div>
 			</header>
 		</div>
 	);
